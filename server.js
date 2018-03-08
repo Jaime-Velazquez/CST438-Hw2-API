650ab1bca7db2379bab4419a56bffd985a1820b0
Jaime Velazquez <jaimegvelazquez@gmail.com>
2018-03-08 11:46:50 +0000

    incorrect github command

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// install express server
+//Install express server
 const express = require('express');
 const app = express();
 
-//serve only the static files from the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-//start the app by listening on the default heroku port
-app.listene(process.env.PORT || 8080);
-
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
